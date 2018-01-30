4ef51f6b4ce8a251c1aa41369ff603610b50930b
Amani Konduru <akonduru2@student.gsu.edu>
2018-01-30 13:16:29 -0500

    Update AntLabMain.java

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -73,6 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("ahudson32");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
+	    	  Person5 p5 = new Person5("Nick Senador");
+                  toPrint += p5.toString("nsenador3");
 
         System.out.println(toPrint);
 
